f75da6e3567af5dbc0afe6589b1e9f04a1c83b15
Zero9x <92634659+Zero9x@users.noreply.github.com>
2021-12-06 09:46:32 +0700

    Add files via upload

---
+++ halo.js
@@ -0,0 +1,21 @@
+﻿/*
+     _ +----------------------------------------------+ _
+    /o)|     
+   / / |         
+  ( (_ |  _                
+ ((\ \)+-/o)--------------------------------------(o\-+(/ /))
+ (\\\ \_/ /                                        \ \_/ ///)
+  \      /                                          \      /
+   \____/  
+ /***** BEGIN CODE Document *****/
+ $(function(){
+    $('html').append('<img id="halo" title="Happy Noel" style="cursor:pointer;position:fixed;z-index:99999" height="120" src="noel/ghost.gif"/>')
+    setInterval(function(){
+       var $X=Math.ceil(Math.random()*$(window).width())
+       var $Y=Math.ceil(Math.random()*$(window).height())
+       $('img#halo').animate({'left':$X,'top':$Y},5000)
+    },5000)
+    $('img#halo').click(function(){
+        window.open('noel/noel.gif','')
+    })
+ })
